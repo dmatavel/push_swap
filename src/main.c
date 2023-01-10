73e67aa3df8100c1529b1a3e101a0ee37bec7906
Danilo Mataveli <dmatavel@pop-os.localdomain>
2023-01-10 13:08:38 -0300

    Handle duplicated elements

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/07 13:43:40 by dmataveç          #+#    #+#             */
-/*   Updated: 2023/01/07 19:22:12 by dmatavel         ###   ########.fr       */
+/*   Created: 2023/01/07 13:43:40 by dmatavel          #+#    #+#             */
+/*   Updated: 2023/01/10 13:01:52 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	main(int argc, char **argv)
 {
+	int	*array;
+
 	if (argc == 1)
 		exit (EXIT_SUCCESS);
 	if (only_integers(argc, argv) != TRUE)
@@ -21,5 +23,12 @@ int	main(int argc, char **argv)
 		put_stderror();
 		exit (EXIT_FAILURE);
 	}
+	array = ft_stoarr((argc - 1), argv);
+	if (has_dup_elements((argc - 1), array))
+	{
+		put_stderror();
+		exit (EXIT_FAILURE);
+	}
+	free(array);
 	return (EXIT_SUCCESS);
 }
